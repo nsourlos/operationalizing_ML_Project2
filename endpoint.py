63fe66b67a90c21556121ab1516f71a5257003f1
Νίκος Σούρλος <soyrlos@gmail.com>
2022-01-19 19:17:23 +0200

    final3

--- endpoint.py
+++ endpoint.py
@@ -3,56 +3,56 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = 'http://dfa3002b-0a7a-4b45-9b68-6d1914f14544.southcentralus.azurecontainer.io/score'
+scoring_uri = 'http://7bbc8877-700c-4035-8097-c35c896039a2.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = '9MWNIDqWS1xPcneM5w671LgipgxrKsWM'
+key = 'Y9bgMCp5O0geM74pEThdQVLiaRMHBjrB'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
         [
           {
             "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
             "job": "blue-collar",
-            "loan": "yes",
             "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
             "month": "may",
-            "nr.employed": 5099.1,
+            "day_of_week": "mon",
+            "duration": 971,
+            "campaign": 1,
             "pdays": 999,
+            "previous": 1,
             "poutcome": "failure",
-            "previous": 1
+            "emp.var.rate": -1.8,
+            "cons.price.idx": 92.893,
+            "cons.conf.idx": -46.2,
+            "euribor3m": 1.299,     
+            "nr.employed": 5099.1,      
           },
           {
             "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
             "job": "blue-collar",
-            "loan": "yes",
             "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
             "month": "may",
-            "nr.employed": 5099.1,
+            "day_of_week": "mon",
+            "duration": 471,
+            "campaign": 1,
             "pdays": 999,
+            "previous": 1,
             "poutcome": "failure",
-            "previous": 1
+            "emp.var.rate": -1.8,
+            "cons.price.idx": 92.893,
+            "cons.conf.idx": -46.2,
+            "euribor3m": 1.299, 
+            "nr.employed": 5099.1,   
           },
       ]
     }
@@ -68,9 +68,5 @@ headers['Authorization'] = f'Bearer {key}'
 
 # Make the request and display the response
 resp = requests.post(scoring_uri, input_data, headers=headers)
+#print(resp)
 print(resp.json())
-
-import time
-time.sleep(10)
-
-
